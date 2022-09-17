4559438778f243567c1a0b31c3ac031b4de8cdb3
olabijed@gmail.com <olabijed@gmail.com>
2022-09-17 18:30:39 +0100

    No content component created

--- app/src/main/java/com/ola/recoverunsold/ui/components/app/NoContentComponent.kt
+++ app/src/main/java/com/ola/recoverunsold/ui/components/app/NoContentComponent.kt
@@ -1,12 +1,16 @@
 package com.ola.recoverunsold.ui.components.app
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
@@ -17,10 +21,21 @@ import com.ola.recoverunsold.R
 fun NoContentComponent(modifier: Modifier = Modifier, message: String) {
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.no_data))
     val progress by animateLottieCompositionAsState(composition)
+    val height = (LocalConfiguration.current.screenHeightDp * 0.33).dp
 
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        LottieAnimation(composition = composition, progress = { progress })
+        LottieAnimation(
+            composition = composition,
+            progress = { progress },
+            modifier = Modifier.height(height),
+            alignment = Alignment.Center,
+            contentScale = ContentScale.Fit
+        )
 
-        Text(text = message, color = MaterialTheme.colors.onBackground)
+        Text(
+            text = message,
+            color = MaterialTheme.colors.onBackground,
+            style = MaterialTheme.typography.h5
+        )
     }
 }
