52f587be4aaf0a501b701c40611b0834660435e0
olabijed@gmail.com <olabijed@gmail.com>
2022-09-22 11:16:28 +0100

    Fixed boolean comparison

--- app/src/main/java/com/ola/recoverunsold/utils/validation/IntegerValidator.kt
+++ app/src/main/java/com/ola/recoverunsold/utils/validation/IntegerValidator.kt
@@ -6,8 +6,7 @@ import com.ola.recoverunsold.utils.resources.Strings
 class IntegerValidator(private val required: Boolean = true) : Validator {
     private val integerRegex = "[0-9]+".toRegex()
 
-    override fun isValid(value: String): Boolean =
-        !required || (required && integerRegex.matches(value))
+    override fun isValid(value: String): Boolean = !required || integerRegex.matches(value)
 
     override fun errorMessage(value: String): String? = if (isValid(value)) {
         null
