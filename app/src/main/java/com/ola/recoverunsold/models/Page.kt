c48f38d4bf738c6145c803dc02bcec15b9ddcb6c
olabijed@gmail.com <olabijed@gmail.com>
2022-11-20 09:40:12 +0100

    Removed unused page properties related to uri

--- app/src/main/java/com/ola/recoverunsold/models/Page.kt
+++ app/src/main/java/com/ola/recoverunsold/models/Page.kt
@@ -9,8 +9,5 @@ data class Page<T>(
     @Json(name = "pageNumber") val pageNumber: Int,
     @Json(name = "pageSize") val pageSize: Int,
     @Json(name = "hasNext") val hasNext: Boolean,
-    @Json(name = "total") val total: Int,
-    @Json(name = "baseUrl") val baseUrl: String? = null,
-    @Json(name = "previousUrl") val previousUrl: String? = null,
-    @Json(name = "nextUrl") val nextUrl: String? = null
+    @Json(name = "total") val total: Int
 )
