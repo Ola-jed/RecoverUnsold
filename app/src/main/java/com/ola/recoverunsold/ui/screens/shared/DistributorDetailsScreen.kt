d5f7d9adfe201e909b5694f56b61110c346720dc
Ola-jed <olabijed@gmail.com>
2023-10-05 15:38:29 +0100

    Add some margin at the bottom of the offers list on the distributor details page

--- app/src/main/java/com/ola/recoverunsold/ui/screens/shared/DistributorDetailsScreen.kt
+++ app/src/main/java/com/ola/recoverunsold/ui/screens/shared/DistributorDetailsScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -479,6 +480,10 @@ fun DistributorDetailsScreen(
                                         page = offers,
                                         onLoadMore = { distributorDetailsViewModel.getNext() }
                                     )
+                                }
+
+                                item {
+                                    Box(modifier = Modifier.height(40.dp))
                                 }
                             }
                         }
